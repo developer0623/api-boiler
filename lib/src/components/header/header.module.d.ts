96fd3dbe1aad3778561d939cf01be10f5c0f8014
developer0623 <developer0623@yahoo.com>
2018-05-28 11:34:30 -0500

    test lib

---
+++ lib/src/components/header/header.module.d.ts
@@ -0,0 +1,6 @@
+import { L10nLoader } from 'angular-l10n';
+export * from './header.component';
+export declare class HeaderModule {
+    l10nLoader: L10nLoader;
+    constructor(l10nLoader: L10nLoader);
+}
